f62f40f176e078fff01eb52ed67d90e9c33ee52d
shravanmukkera <shravan.mukkera@cjsstechnologies.com>
2023-03-03 15:30:35 +0530

    no message

--- project2/src/Main.java
+++ project2/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("shravan 2nd project");
+        System.out.println("shravan 2nd project updated...");
     }
 }
